9538966952f43923cda467cede204f5d5e992d8c
O H <olle@unremarkablegarden.com>
2020-09-18 13:25:07 +0200

    honk

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,31 +1,38 @@
 <template lang="pug">
   #logo
     .bg
-      img(src='~assets/img/logo-bg.svg')
+      span &nbsp;
+      //- img(src='~assets/img/logo-bg.svg')
     .fg
       img(src='~assets/img/logo.svg')
 </template>
 
 <style lang="sass">
 #logo
-  position: absolute
+  // position: absolute
+  position: fixed
   right: 3vw
   top: 3vw
   width: 18vw
-  .bg img
+  .bg 
+    // img
     position: relative
     top: 0vw
     right: 0vw
-    width: 18vw
-    height: auto
+    width: 17vw
+    height: 18vw
     z-index: 88
+    background: rgba(10,10,10,0.12)
+    mask: url(~assets/img/logo-bg.svg) no-repeat center
+    // border: 1px blue solid
   .fg img
     position: relative
-    top: 0.3vw
-    right: -1.3vw
+    top: 1.3vw
+    right: -2vw
     width: 15vw
     height: auto
     z-index: 99
+    opacity: 0.9
   .bg
     position: absolute
     
