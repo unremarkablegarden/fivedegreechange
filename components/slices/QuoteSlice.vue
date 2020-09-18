a0857ed7d801f2ad50e211ec5698c925b5329cdd
O H <olle@unremarkablegarden.com>
2020-09-18 15:06:44 +0200

    honk

--- components/slices/QuoteSlice.vue
+++ components/slices/QuoteSlice.vue
@@ -1,6 +1,6 @@
 <template lang='pug'> 
   .post-part.single.container
-    blockquote.block-quotation.
+    blockquote(:class='slice.primary.align.toLowerCase()').block-quotation.
       {{ $prismic.asText(slice.primary.quote) }}
     
 </template>
@@ -17,9 +17,6 @@ export default {
   // margin-bottom: 2rem
   // margin: 2rem 12rem 3rem 6rem
   padding-top: 1rem
-  margin: -1rem 0 1.7rem 2rem
-  float: right
-  width: 40%
   font-size: 3.6vw
   line-height: 1.2em
   letter-spacing: -0.06em
@@ -28,12 +25,35 @@ export default {
   color: #FF3F16
   // color: #6C91F7
   // opacity: 0.45
+  &.right
+    margin: -1rem 0 1.7rem 2rem
+    float: right
+    width: 40%
+  &.left
+    float: left
+    margin: -1rem 2rem 1.7rem 0
+    width: 30%
+  &.center
+    width: 100%
+    text-align: center
+    padding: 3vw 0 4vw
+    margin: 4vw 0
+    // background: rgba(0,0,0,0.1)
+    color: rgba(0,0,0,0.9)
+    border: 2px rgba(0,0,0,0.2) solid
+    border-left: 0
+    border-right: 0
+    line-height: 1.8em
   
   &:before
-    content: "» "
-    // margin-left: -2rem
+    content: "»"
+    opacity: 0.5
+    padding-right: 0.2em
   &:after
     content: "«"
+    opacity: 0.5
+    padding-left: 0.2em
+    padding-right: 0.5em
 .block-citation
   display: inline-block
   font-style: italic
